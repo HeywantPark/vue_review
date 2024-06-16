294303d25d9bae13d13526bee185f2eac195182b
Park Hyewon <db1648@naver.com>
2024-06-16 15:11:50 +0900

    240616 definprops를 사용하여 자식컴퍼넌트에 메세지 전달

---
+++ 09.compositionAPI/composition_API2/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <defineProps :msg="msg" />
+  </div>
+</template>
+
+<script setup>
+import defineProps from './components/defineProps.vue';
+import { ref } from 'vue';
+
+const msg = ref('props에게 전할 메세지');
+</script>
