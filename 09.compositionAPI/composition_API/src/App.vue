221280e608b4e6b818a360c36bfeaa4ffbef0bc9
Park Hyewon <db1648@naver.com>
2024-06-16 13:37:01 +0900

    240616 vue3문법을 사용한 counter 예제문제 풀이

---
+++ 09.compositionAPI/composition_API/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>vue3 counter</h1>
+    <p>count : {{ count }}</p>
+    <p>double : {{ double }}</p>
+    <p>지금까지 계산된 횟수 : {{ clickCount }}</p>
+    <button @click="incrememt">incrememt</button>
+    <button @click="decrememt">decrememt</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const count = ref(0);
+const double = computed(() => count.value * 2);
+const clickCount = ref(0);
+
+function incrememt() {
+  count.value++;
+  clickCount.value++;
+}
+function decrememt() {
+  count.value--;
+  clickCount.value++;
+}
+</script>
+
+<style lang="scss" scoped></style>
